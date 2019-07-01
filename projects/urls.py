9ec3e29220abbb5f549b419ad6876055f2528e95
tgalvinjr <tgalvin.jr@gmail.com>
2019-07-01 14:01:34 +0300

    update urls.py

--- projects/urls.py
+++ projects/urls.py
@@ -7,7 +7,6 @@ urlpatterns=[
     url(r'^$',views.index,name='Index'),
     url(r'^create/profile$',views.create_profile, name='create-profile'),
     url(r'^new/project$',views.new_project, name='new-project'),
-    url(r'^directory/',views.directory, name='directory'),
     url(r'^profile/',views.profile, name='profile'),
     url(r'^site/(\d+)',views.site,name='site'),
     url(r'^search/',views.search_results, name='search_results'),
