e8cafa4186ace48259f6920b3c6fff1a9d33d97b
tgalvinjr <tgalvin.jr@gmail.com>
2019-07-01 12:54:38 +0300

    create django project

---
+++ projects/serializer.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Project
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        # model = UserProfile
+        fields = ('user','bio','profile_pic','projects')
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ('project_title','project_description','landing_page','live_site','design','usability','content')
